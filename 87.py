1296b929612d7b1b670fb765e4604d9a9ca3dff0
DoliDoli <basrooom@gmail.com>
2016-10-03 14:25:39 +0900

    Update 87.py

--- 87.py
+++ 87.py
@@ -10,7 +10,7 @@ b = []
 
 if __name__ == "__main__":
 
-　# aに"UnitedStates"の300次元ベクトルを入れる
+　## aに"UnitedStates"の300次元ベクトルを入れる
   for line in fin: #読み込み
     string1 = re.split(" ",line[:-1])
     if string1[0] == "United_States":
@@ -18,7 +18,7 @@ if __name__ == "__main__":
       for x in string1[1:301]:
         a.append(float(x))
       break
-　# bに"U.S"の300次元ベクトルを入れる
+　## bに"U.S"の300次元ベクトルを入れる
   fin = codecs.open('85vec.txt', 'r', 'utf_8')
   for line in fin: #読み込み
     string2 = re.split(" ",line[:-1])
@@ -30,6 +30,8 @@ if __name__ == "__main__":
 
   npa = np.array(a)
   npb = np.array(b)
-  # コサイン距離を求めている
+  ## コサイン距離を求めている
+  ## np.dot →　ベクトルの内積
+  ## np.linalg.norm() →　ベクトルの長さ 
   cos_sim = np.dot(npa, npb) / ( np.linalg.norm(npa) * np.linalg.norm(npb) )
   print(cos_sim)
